e30916ae986390858256a8537954d06227e6db7a
Bayarmaa <b.bayarmaa0321@gmail.com>
2026-01-27 13:58:39 +0530

    Initial exam platform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,10 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "k8s" {
+resource "aws_instance" "exam_server" {
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
+  tags = {
+    Name = "exam-k8s-node"
+  }
 }
